71baf32739387635a37304f533847c3b7a5fa0f9
anchibaby0323-cmyk <anchibaby0323@gmail.com>
2026-09-05 21:02:05 +0800

    Fix Compose verticalScroll import

--- app/src/main/java/com/novatune/engine/ui/GamingDashboardScreen.kt
+++ app/src/main/java/com/novatune/engine/ui/GamingDashboardScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -24,7 +25,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.verticalScroll
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
