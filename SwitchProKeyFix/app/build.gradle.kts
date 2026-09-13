d05ef95c7209e709aff56192212c1e02183b508e
anchibaby0323-cmyk <anchibaby0323@gmail.com>
2026-09-13 10:16:56 +0800

    Bump SwitchProKeyFix to v0.1.4

--- SwitchProKeyFix/app/build.gradle.kts
+++ SwitchProKeyFix/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.switchprokeyfix"
         minSdk = 30
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.1.3"
+        versionCode = 5
+        versionName = "0.1.4"
     }
 
     buildFeatures {
