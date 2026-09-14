81225462d0593ccf8ac35fa77d2a28e7f248f3e1
anchibaby0323-cmyk <anchibaby0323@gmail.com>
2026-09-14 17:46:45 +0800

    Add Bridge app Gradle config

---
+++ AIOS-GPT-Gemini-Minis-Bridge/app/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "com.capybara.gptgeminiminisbridge"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.capybara.gptgeminiminisbridge"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0-alpha"
+    }
+}
