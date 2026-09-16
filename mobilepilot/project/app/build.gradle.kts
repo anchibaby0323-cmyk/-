64766a4d6f5d0613724ef176afc74d81a4747fd4
anchibaby0323-cmyk <anchibaby0323@gmail.com>
2026-09-16 17:04:40 +0800

    Release MobilePilot 0.2 bridge configuration

--- mobilepilot/project/app/build.gradle.kts
+++ mobilepilot/project/app/build.gradle.kts
@@ -4,14 +4,14 @@ plugins {
 
 android {
     namespace = "dev.mobilepilot"
-    compileSdk = 36
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "dev.mobilepilot"
         minSdk = 26
-        targetSdk = 36
-        versionCode = 1
-        versionName = "0.1.0"
+        targetSdk = 35
+        versionCode = 2
+        versionName = "0.2.0-bridge"
     }
 
     buildTypes {
