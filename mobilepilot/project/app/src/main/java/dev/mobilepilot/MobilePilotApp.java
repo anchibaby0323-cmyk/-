fdff1353a9463a604228108eed28fb267f496e42
anchibaby0323-cmyk <anchibaby0323@gmail.com>
2026-09-16 17:39:52 +0800

    MobilePilot: track Shizuku and notification access

--- mobilepilot/project/app/src/main/java/dev/mobilepilot/MobilePilotApp.java
+++ mobilepilot/project/app/src/main/java/dev/mobilepilot/MobilePilotApp.java
@@ -2,10 +2,12 @@ package dev.mobilepilot;
 
 import android.app.Application;
 import dev.mobilepilot.core.AuditLog;
+import dev.mobilepilot.shizuku.ShizukuBridge;
 
 public class MobilePilotApp extends Application {
     @Override public void onCreate() {
         super.onCreate();
         AuditLog.init(this);
+        ShizukuBridge.initialize();
     }
 }
